95e0b38ac1d1f95e07d4dd587929f226269cd261
Cory Kane <ckane30691@gmail.com>
2018-06-09 15:59:17 -0700

    Get react skeleton up and running

---
+++ frontend/components/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
+import Splash from "./greeting/splash";
+
+const App = () => (
+    <div>
+        <Switch>
+            <Route exact path="/" component={Splash} />
+        </Switch>
+    </div>
+);
+
+export default App;
---
+++ frontend/components/greeting/splash.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Splash extends React.Component {
+    constructor() {
+      super();
+    }
+
+    render() {
+        return <h1>"Test"</h1>;
+    }
+}
+
+export default Splash;
